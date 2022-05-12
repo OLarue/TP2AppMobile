55b013302d0c5705873ca297be853e48a872ad23
KatsuroDev <katsuro.userg@gmail.com>
2022-05-12 16:45:54 -0400

    add timer for loading

--- app/src/main/java/ca/qc/cstj/tp2/presentation/ui/loading/LoadingActivity.kt
+++ app/src/main/java/ca/qc/cstj/tp2/presentation/ui/loading/LoadingActivity.kt
@@ -1,4 +1,33 @@
 package ca.qc.cstj.tp2.presentation.ui.loading
 
-class LoadingActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.viewbinding.library.activity.viewBinding
+import androidx.activity.viewModels
+import ca.qc.cstj.tp2.R
+import ca.qc.cstj.tp2.core.Constants
+import ca.qc.cstj.tp2.databinding.ActivityLoadingBinding
+
+class LoadingActivity: AppCompatActivity() {
+
+    private val binding by viewBinding<ActivityLoadingBinding>()
+    private val viewModel by viewModels<LoadingViewModel>()
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        viewModel.loadingProgress.observe(this) {
+            binding.txvLoading.text = getString(R.string.LoadingText, it, Constants.LOADING_MAX)
+            binding.pgbLoading.setProgress(it, true)
+        }
+    }
 }
+
+
+
+
+
--- app/src/main/java/ca/qc/cstj/tp2/presentation/ui/loading/LoadingViewModel.kt
+++ app/src/main/java/ca/qc/cstj/tp2/presentation/ui/loading/LoadingViewModel.kt
@@ -1,4 +1,30 @@
 package ca.qc.cstj.tp2.presentation.ui.loading
 
-class LoadingViewModel {
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoadingViewModel : ViewModel() {
+    private val _loadingProgress = MutableLiveData<Int>()
+    val loadingProgress: LiveData<Int> get() = _loadingProgress
+
+    private val timer = object: CountDownTimer(10000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            incrementProgress()
+        }
+
+        override fun onFinish() {
+            // Change activity
+        }
+    }
+
+    init {
+        _loadingProgress.value = 0
+        timer.start()
+    }
+
+    fun incrementProgress() {
+        _loadingProgress.value = _loadingProgress.value!!.plus(1)
+    }
 }
