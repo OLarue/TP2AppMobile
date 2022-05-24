b654c039875f28aaf29cc0dd2ff423498d483b00
KatsuroDev <katsuro.userg@gmail.com>
2022-05-24 16:30:54 -0400

    Feature/gateways fragment (#6)
    * add item layout
    
    * fix item layout missing part
    
    * add recycler view adapter
    
    * add repository
    
    * add updating list
    
    * fix item width
    
    * add binding for items
    
    * fix refresh rate name
    
    * add loading progress

--- app/src/main/java/ca/qc/cstj/tp2/presentation/ui/gateways/GatewaysViewModel.kt
+++ app/src/main/java/ca/qc/cstj/tp2/presentation/ui/gateways/GatewaysViewModel.kt
@@ -1,4 +1,30 @@
 package ca.qc.cstj.tp2.presentation.ui.gateways
 
-class GatewaysViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ca.qc.cstj.tp2.core.LoadingResource
+import ca.qc.cstj.tp2.data.repositories.GatewayRepository
+import ca.qc.cstj.tp2.domain.models.Gateway
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class GatewaysViewModel : ViewModel() {
+    private val gatewayRepository = GatewayRepository()
+
+    private val _gateways = MutableLiveData<LoadingResource<List<Gateway>>>()
+    val gateways : LiveData<LoadingResource<List<Gateway>>> get() = _gateways
+
+    init {
+        refreshGateways()
+    }
+
+    private fun refreshGateways() {
+        viewModelScope.launch {
+            gatewayRepository.retrieveAll().collect {
+                _gateways.value = it
+            }
+        }
+    }
 }
