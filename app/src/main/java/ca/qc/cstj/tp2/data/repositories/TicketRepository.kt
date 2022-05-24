1893caacb59f18a09ab6a1a45179f9d33ddcc2f9
1950658 <lajpie@gmail.com>
2022-05-24 09:34:14 -0400

    TicketDataSource.kt et TicketRepository.kt

--- app/src/main/java/ca/qc/cstj/tp2/data/repositories/TicketRepository.kt
+++ app/src/main/java/ca/qc/cstj/tp2/data/repositories/TicketRepository.kt
@@ -1,4 +1,21 @@
 package ca.qc.cstj.tp2.data.repositories
 
+import ca.qc.cstj.tp2.core.LoadingResource
+import ca.qc.cstj.tp2.data.datasources.TicketDataSource
+import ca.qc.cstj.tp2.domain.models.Ticket
+import kotlin.Exception
+
 class TicketRepository {
+
+    private val ticketDataSource =  TicketDataSource()
+
+    suspend fun retrieveAll(): LoadingResource<List<Ticket>> {
+        return  try {
+            LoadingResource.Loading()
+            LoadingResource.Success(ticketDataSource.retrieveAll())
+        } catch (ex: Exception) {
+            LoadingResource.Error(ex)
+        }
+    }
+
 }
