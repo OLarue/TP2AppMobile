d410757d897db186e55b67abf1ec7c75d7133c16
1950658 <lajpie@gmail.com>
2022-05-24 10:04:28 -0400

    TicketRepository.kt et TicketsViewModel.kt + nouv dans  Constants.kt

--- app/src/main/java/ca/qc/cstj/tp2/data/repositories/TicketRepository.kt
+++ app/src/main/java/ca/qc/cstj/tp2/data/repositories/TicketRepository.kt
@@ -1,21 +1,32 @@
 package ca.qc.cstj.tp2.data.repositories
 
+import ca.qc.cstj.tp2.core.Constants
 import ca.qc.cstj.tp2.core.LoadingResource
 import ca.qc.cstj.tp2.data.datasources.TicketDataSource
 import ca.qc.cstj.tp2.domain.models.Ticket
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlin.Exception
 
 class TicketRepository {
 
     private val ticketDataSource =  TicketDataSource()
 
-    suspend fun retrieveAll(): LoadingResource<List<Ticket>> {
-        return  try {
-            LoadingResource.Loading()
-            LoadingResource.Success(ticketDataSource.retrieveAll())
-        } catch (ex: Exception) {
-            LoadingResource.Error(ex)
+    suspend fun retrieveAll(): Flow<LoadingResource<List<Ticket>>> {
+        return  flow{
+            while (true)
+            {
+                try {
+                    emit(LoadingResource.Loading())
+                    emit(LoadingResource.Success(ticketDataSource.retrieveAll()))
+                } catch (ex: Exception){
+                    emit(LoadingResource.Error(ex,"erreur serveur"))
+                }
+                delay(Constants.RefreshRates.TICKET_REFRESH_RATE)
+            }
         }
     }
+
 
 }
