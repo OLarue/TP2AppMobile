203b7c6ac240ff8c79f7965471204f5e0219a932
olarue <1765918@cstj.qc.ca>
2022-05-31 15:57:12 -0400

    Ajouter un peu de code pour la partie a P-A pour l'avancé un peu et le faire partir en feu. Also added la direction vers le gateway detail.

--- app/src/main/java/ca/qc/cstj/tp2/presentation/ui/gateways/detail/DetailGatewayFragment.kt
+++ app/src/main/java/ca/qc/cstj/tp2/presentation/ui/gateways/detail/DetailGatewayFragment.kt
@@ -1,7 +1,18 @@
 package ca.qc.cstj.tp2.presentation.ui.gateways.detail
 
+import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import ca.qc.cstj.tp2.R
+import ca.qc.cstj.tp2.databinding.FragmentDetailGatewayBinding
 
 class DetailGatewayFragment : Fragment(R.layout.fragment_detail_gateway){
+    private val binding: FragmentDetailGatewayBinding by viewBinding()
+    private val viewModel: DetailGatewayViewModel by viewModels{
+        DetailGatewayViewModel.Factory(args.href)
+    }
+
+    private val args : DetailGatewayFragmentArgs by navArgs()
+
 }
--- app/src/main/java/ca/qc/cstj/tp2/presentation/ui/gateways/detail/DetailGatewayViewModel.kt
+++ app/src/main/java/ca/qc/cstj/tp2/presentation/ui/gateways/detail/DetailGatewayViewModel.kt
@@ -1,4 +1,24 @@
 package ca.qc.cstj.tp2.presentation.ui.gateways.detail
 
-class DetailGatewayViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class DetailGatewayViewModel (private val href:String) : ViewModel(){
+
+
+
+    init{
+        viewModelScope.launch {
+
+        }
+    }
+
+    //Pour le constructeur qui prend un parametre
+    class Factory(private val href:String) : ViewModelProvider.Factory{
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return modelClass.getConstructor(String::class.java).newInstance(href)
+        }
+    }
 }
